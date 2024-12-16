cb51116465002671f8f88165d04b292568102523
Miaofei <ameision@hotmail.com>
2024-12-15 23:59:57 -0800

    change default controller keys config
    Signed-off-by: Miaofei <ameision@hotmail.com>

--- src/lib/destruct.c
+++ src/lib/destruct.c
@@ -67,7 +67,7 @@
 
 /*** Defines ***/
 #define UNIT_HEIGHT 12
-#define MAX_KEY_OPTIONS 4
+#define MAX_KEY_OPTIONS 2
 
 /*** Enums ***/
 enum de_state_t
@@ -463,24 +463,24 @@ static const JE_byte ModeScore[MAX_PLAYERS][MAX_MODES] =
 static SDL_Scancode defaultKeyConfig[MAX_PLAYERS][MAX_KEY][MAX_KEY_OPTIONS] =
 {
     {
-        {SDL_SCANCODE_C},
-        {SDL_SCANCODE_V},
-        {SDL_SCANCODE_A},
-        {SDL_SCANCODE_Z},
-        {SDL_SCANCODE_LALT},
-        {SDL_SCANCODE_X, SDL_SCANCODE_LSHIFT},
-        {SDL_SCANCODE_LCTRL},
-        {SDL_SCANCODE_SPACE}
+        {SDL_SCANCODE_F},           // LEFT
+        {SDL_SCANCODE_H},           // RIGHT
+        {SDL_SCANCODE_T},           // UP
+        {SDL_SCANCODE_G},           // DOWN
+        {SDL_SCANCODE_Y},           // CHANGE_UNIT
+        {SDL_SCANCODE_A},           // FIRE
+        {SDL_SCANCODE_D},           // CHANGE_WEAPON_UP
+        {SDL_SCANCODE_S}            // CHANGE_WEAPON_DOWN
     },
     {
-        {SDL_SCANCODE_LEFT, SDL_SCANCODE_KP_4},
-        {SDL_SCANCODE_RIGHT, SDL_SCANCODE_KP_6},
-        {SDL_SCANCODE_UP, SDL_SCANCODE_KP_8},
-        {SDL_SCANCODE_DOWN, SDL_SCANCODE_KP_2},
-        {SDL_SCANCODE_BACKSLASH, SDL_SCANCODE_KP_5},
-        {SDL_SCANCODE_INSERT, SDL_SCANCODE_RETURN, SDL_SCANCODE_KP_0, SDL_SCANCODE_KP_ENTER},
-        {SDL_SCANCODE_PAGEUP, SDL_SCANCODE_KP_9},
-        {SDL_SCANCODE_PAGEDOWN, SDL_SCANCODE_KP_3}
+        {SDL_SCANCODE_KP_4},        // LEFT
+        {SDL_SCANCODE_KP_6},        // RIGHT
+        {SDL_SCANCODE_KP_8},        // UP
+        {SDL_SCANCODE_KP_5},        // DOWN
+        {SDL_SCANCODE_KP_9},        // CHANGE_UNIT
+        {SDL_SCANCODE_SEMICOLON},   // FIRE
+        {SDL_SCANCODE_RETURN},      // CHANGE_WEAPON_UP
+        {SDL_SCANCODE_APOSTROPHE}   // CHANGE_WEAPON_DOWN
     }
 };
 
